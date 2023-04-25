5fa3cdd7a0b2fbc778224a3ccfd33144ad149868
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-04-25 22:56:31 +0200

    installation basique du blog
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 3, maximum: 100 }
+  validates :content, presence: true, length: { minimum: 10 }
+
+  has_rich_text :content
 end
