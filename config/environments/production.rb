809edc8b700ab02f7e69616a2f080868edd9bacc
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-04-30 20:33:11 +0200

    ajout gem terser pour compresser en prod pour meilleure perf
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,9 +31,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.middleware.use Rack::Brotli
-
+  config.assets.js_compressor = Terser::Compressor.new(harmony: true)
 
   config.middleware.use HtmlCompressor::Rack,
     compress_css: true,
